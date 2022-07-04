bf12f277753b2cccf122f63d82baaa86201d611f
Kardelen Geçkin <101407832+kgeckin@users.noreply.github.com>
2022-07-05 01:29:38 +0300

    Update country_vaccinations_dataset_codes.py

--- country_vaccinations_dataset_codes.py
+++ country_vaccinations_dataset_codes.py
@@ -1,13 +1,11 @@
 import pandas as pd
-
-df_data_1 = pd.read_csv(body)
-df_data_1.head()
+df = pd.read_csv('country_vaccination_stats.csv')
 
 
 # Implement code to fill the missing data (impute) in daily_vaccinations column per country with the minimum daily vaccination number of relevant countries.
 # Note: If a country does not have any valid vaccination number yet, fill it with “0” (zero).
-df_data_1['daily_vaccinations']=df_data_1['daily_vaccinations'].replace(np.nan, 0).astype(int)
-df_data_1.head()
+df["daily_vaccinations"]=df["daily_vaccinations"].replace(np.nan, 0).astype(int)
+df.head() # shows the first 5 columns
 
 # Implement code to list the top-3 countries with highest median daily vaccination numbers by considering missing values imputed version of dataset.
 df_data_1.nlargest(3, 'daily_vaccinations')
